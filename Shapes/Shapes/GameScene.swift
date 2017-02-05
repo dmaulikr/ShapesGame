0e58471b1dc9f0dcc8f3126200cfd47ec249cfe6
Shabir Jan <iam.shabir@yahoo.com>
2017-02-05 23:44:22 +0500

    Added code for initial circle

--- Shapes/Shapes/GameScene.swift
+++ Shapes/Shapes/GameScene.swift
@@ -15,75 +15,44 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
+        setupPlayerAndObstacles()
+    }
+    
+    func setupPlayerAndObstacles(){
+        addObstacle()
+    }
+    func addObstacle()
+    {
+        addCircleObstacle()
+    }
+    
+    func addCircleObstacle() {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let path = UIBezierPath()
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        path.move(to: CGPoint(x: 0, y:-200))
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        path.addLine(to: CGPoint(x: 0, y: -160))
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        path.addArc(withCenter: CGPoint.zero, radius: 160, startAngle: CGFloat(3.0 * M_PI_2), endAngle: CGFloat(0), clockwise: true)
+        
+        path.addLine(to: CGPoint(x:200,y:0))
+        
+        path.addArc(withCenter: CGPoint.zero, radius: 200, startAngle: CGFloat(0), endAngle: CGFloat(3.0 * M_PI_2), clockwise: false)
+        
+        let section = SKShapeNode(path: path.cgPath)
+        section.position = CGPoint(x:size.width/2, y:size.height/2)
+        section.fillColor = .yellow
+        section.strokeColor = .yellow
+        addChild(section)
+        
+        let section2 = SKShapeNode(path: path.cgPath)
+        section2.position = CGPoint(x: size.width/2, y: size.height/2)
+        section2.fillColor = .red
+        section2.strokeColor = .red
+        section2.zRotation = CGFloat(M_PI)
+        addChild(section2)
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
